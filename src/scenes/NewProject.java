e8ef2e6df7c46d9d92187484026b14845c123648
Deus <rogerferraro256@gmail.com>
2018-08-13 21:40:49 -0300

    arrumação na tela de novo projeto

--- src/scenes/NewProject.java
+++ src/scenes/NewProject.java
@@ -2,6 +2,7 @@ package scenes;
 
 
 import javafx.geometry.Pos;
+import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.TextArea;
 import javafx.scene.control.Hyperlink;
@@ -10,7 +11,7 @@ import javafx.scene.control.TextField;
 import javafx.scene.layout.HBox;
 import javafx.scene.layout.VBox;
 
-public class NewProject extends VBox {
+public class NewProject extends Scene {
 
 	private Label lblProjectName, lblDescProject, lblMember;
 	private TextField txtProjectName,txtMember;
@@ -18,9 +19,10 @@ public class NewProject extends VBox {
 	private TextArea txtDescProject;
 	private Button btnSalvar,btnCancelar,btnInvite;
 	private HBox hbxPN,hbxBTN,hbxMember;
-	
+	private VBox vbxTela;
 	
 	public NewProject(){	
+		super(new VBox());
 		this.lblProjectName = new Label("Nome do Projeto");
 		this.lblDescProject = new Label("Descrição do Projeto");
 		this.lblMember = new Label("Membros:");
@@ -31,6 +33,7 @@ public class NewProject extends VBox {
 		this.hbxBTN = new HBox();
 		this.hbxPN = new HBox();
 		this.hbxMember = new HBox();
+		this.vbxTela = new VBox();
 		
 		this.txtProjectName.setMaxWidth(300);
 		this.txtDescProject.setMaxWidth(300);
@@ -48,9 +51,11 @@ public class NewProject extends VBox {
 		hbxMember.getChildren().addAll(lblMember,txtMember,btnInvite);
 		
 		
-		this.getChildren().addAll(hbxPN,lblDescProject,txtDescProject,hbxMember,lblAddMember);
-		this.getChildren().addAll(hbxBTN);
-		this.setAlignment(Pos.CENTER);
-		this.setSpacing(5);
+		vbxTela.getChildren().addAll(hbxPN,lblDescProject,txtDescProject,hbxMember,lblAddMember);
+		vbxTela.getChildren().addAll(hbxBTN);
+		vbxTela.setAlignment(Pos.CENTER);
+		vbxTela.setSpacing(5);
+		
+		setRoot(vbxTela);
 	}
 }
