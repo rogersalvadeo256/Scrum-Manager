8955c7b3d5c32f06df49c95d9a28d90cd222d12a
jefter santiago <jefter.santiago66@gmail.com>
2018-09-15 21:39:06 -0500

    terminando a tela esqueci minha senha

--- src/application/main/Window.java
+++ src/application/main/Window.java
@@ -3,22 +3,19 @@ package application.main;
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
+import java.io.IOException;
 import java.sql.SQLException;
 
-import db.hibernate.factory.Database;
 import javafx.scene.image.Image;
 import javafx.stage.Stage;
 import scenes.popoups.ForgotPasswordPOPOUP;
-import scenes.popoups.FriendshipRequestPOPOUP;
-import scenes.popoups.ProfileEditPOPOUP;
-import scenes.scenes.LoginScene;
 
 public class Window extends Stage {
 	
 	public static Stage mainStage;
 	private FileInputStream fis;
 	
-	public Window() throws ClassNotFoundException, SQLException, FileNotFoundException {
+	public Window() throws ClassNotFoundException, SQLException, IOException {
 		Window.mainStage = this;
 		
 		this.fis = new FileInputStream(new File("resources/images/icons/scrum_icon.png"));
@@ -28,11 +25,12 @@ public class Window extends Stage {
 		//
 		
 		// mainStage.setScene(new HomePageScene());
-		// mainStage.setScene(new NewProjectScene());
-		mainStage.setScene(new LoginScene());
+//		 mainStage.setScene(new NewProjectScene());
+//		mainStage.setScene(new LoginScene());	
 		
-//		ForgotPasswordPOPOUP test = new ForgotPasswordPOPOUP(this);
-//		test.show();
+		
+		ForgotPasswordPOPOUP test = new ForgotPasswordPOPOUP(this);
+		test.show();
 		
 		// ProfileEditPOPOUP tests = new ProfileEditPOPOUP(this);
 		// tests.show();
--- src/scenes/popoups/ForgotPasswordPOPOUP.java
+++ src/scenes/popoups/ForgotPasswordPOPOUP.java
@@ -9,6 +9,7 @@ import javax.persistence.Query;
 import db.hibernate.factory.Database;
 import db.pojos.UserRegistration;
 import db.util.ProfileImg;
+import javafx.application.Platform;
 import javafx.beans.value.ChangeListener;
 import javafx.beans.value.ObservableValue;
 import javafx.geometry.Pos;
@@ -27,20 +28,21 @@ import javafx.scene.layout.VBox;
 import javafx.stage.Modality;
 import javafx.stage.Stage;
 import javafx.stage.Window;
+import listeners.Close;
 import validation.CheckEmptyFields;
 import widgets.alertMessage.CustomAlert;
 import widgets.designComponents.HBProfileContentForgotPassword;
 
 public class ForgotPasswordPOPOUP extends Stage {
 	
-	private Label lblQuestion, lblEmail, lblNPass, lblCNpass;
-	private TextField txtEmail;
-	private PasswordField npass, cnpass;
+	private Label lblQuestion, lblAnswer, lblEmailOrUsername, lblNewPasswrd, lblConfirmPassword;
+	private TextField txtEmailUserName,txtAnswer;
+	private PasswordField newPassword, passwordConfirmation;
 	private EntityManager em;
 	private Query q;
 	private VBox layout;
 	private Scene scene;
-	private Button btnOk, btnCancel;
+	private Button btnOk, btnCancel,btnEmail;
 	private HBox hbButtons;
 	private ToggleButton tbYes, tbNot;
 	private HBProfileContentForgotPassword hbp;
@@ -48,35 +50,26 @@ public class ForgotPasswordPOPOUP extends Stage {
 	private CheckEmptyFields checkFields;
 	private ImageView img;
 	private ArrayList<String> message;
-	private boolean needEmail = false;
-	public ForgotPasswordPOPOUP(Window owner) {
+
+	public ForgotPasswordPOPOUP(Window owner) throws IOException {
 		init(owner);
-		drawNegativeAnswer();
 	}
 	
-	public ForgotPasswordPOPOUP(Window owner, TextField userNameOrEmail) throws IOException {
-		init(owner);
-		if (valideEmail(userNameOrEmail)) {
-			this.layout.getChildren().add(hbp);
-			this.layout.getChildren().addAll(this.lblQuestion, hbButtons);
-		}
-	}
-	
-	public <T> void init(Window owner) {
+	public void init(Window owner) {
 		
 		this.checkFields = new CheckEmptyFields();
 		this.message = new ArrayList<String>();
 		
 		this.lblQuestion = new Label("Este(ª) é voce?");
 		
-		this.lblEmail = new Label("Digite seu email: ");
-		this.txtEmail = new TextField();
+		this.lblEmailOrUsername = new Label("Digite seu email ou nome de usuario ");
+		this.txtEmailUserName = new TextField();
 		
-		this.npass = new PasswordField();
-		this.lblNPass = new Label("Digite uma nova senha: ");
+		this.newPassword = new PasswordField();
+		this.lblNewPasswrd = new Label("Digite uma nova senha: ");
 		
-		this.lblCNpass = new Label("Confirmação de senha: ");
-		this.cnpass = new PasswordField();
+		this.lblConfirmPassword = new Label("Confirmação de senha: ");
+		this.passwordConfirmation = new PasswordField();
 		
 		this.hbButtons = new HBox();
 		
@@ -87,27 +80,11 @@ public class ForgotPasswordPOPOUP extends Stage {
 		this.hbButtons.getChildren().addAll(tbNot, tbYes);
 		
 		this.btnCancel = new Button("Cancelar");
-		this.btnCancel.setOnAction(e -> {
-			
-		});
+		this.btnCancel.setOnAction(e-> { this.close();});
 		this.btnOk = new Button("Ok");
 		this.btnOk.setOnAction(e -> {
-			if (!valideFieldsEmpty()) {
-				if (this.q.getResultList().isEmpty()) {
-					if (!(changePassword(txtEmail.getText(), npass.getText(), cnpass.getText(), true))){ 
-						new CustomAlert(AlertType.ERROR, "ERRO", "Algo errado", "Senhas não correspondem");
-					}else { 
-						new CustomAlert(AlertType.CONFIRMATION, "Pronto", "Senha alterada", "Sua senha foi alterada");
-					}
-				} else { 
-					if(!(changePassword(txtEmail.getText(), npass.getText(), cnpass.getText(), false))){
-						new CustomAlert(AlertType.ERROR, "ERRO", "Algo errado", "Senhas não correspondem");
-					} else { 
-						new CustomAlert(AlertType.CONFIRMATION, "Pronto", "Senha alterada", "Sua senha foi alterada");
-					}
-				}
-			}
 		});
+
 		this.tbGroup = new ToggleGroup();
 		this.tbNot.setToggleGroup(tbGroup);
 		this.tbYes.setToggleGroup(tbGroup);
@@ -116,22 +93,25 @@ public class ForgotPasswordPOPOUP extends Stage {
 			public void changed(ObservableValue<? extends Toggle> old_value, Toggle toggle,
 					Toggle new_toggle) {
 				if (tbGroup.getSelectedToggle() == tbNot) {
-					needEmail = true;
-					drawNegativeAnswer();
 				}
 				if (tbGroup.getSelectedToggle() == tbYes) {
-					needEmail = false;
-					try {
-						drawPositiveAnswser();
-					} catch (IOException e) {
-						e.printStackTrace();
-					}
 				}
 			}
 		});
-		
 		this.layout = new VBox(10);
 		this.scene = new Scene(layout);
+		
+		this.btnEmail=new Button("Enviar");
+		
+		this.layout.getChildren().addAll(this.lblEmailOrUsername, this.txtEmailUserName,this.btnEmail);
+		btnEmail.setOnAction(e -> { 
+			
+			
+			
+		});
+		
+		
+		
 		
 		this.layout.setAlignment(Pos.CENTER);
 		
@@ -143,148 +123,47 @@ public class ForgotPasswordPOPOUP extends Stage {
 		this.setHeight(550);
 		this.setResizable(false);
 	}
-	
-	/**
-	 * if the return are false, none email was found if the return are true, the
-	 * email is valid
-	 * 
-	 * @param  TextField
-	 *                            email
-	 * @return             boolean
-	 * @throws IOException
-	 */
-	private boolean valideEmail(TextField emailOrUserName) throws IOException {
-		if (em == null)
-			em = Database.createEntityManager();
-		this.q = em.createQuery("from UserRegistration where email =: email or userName =:userName");
-		q.setParameter("email", emailOrUserName.getText());
-		q.setParameter("userName", emailOrUserName.getText());
-		if (q.getResultList().isEmpty()) {
-			em = null;
-			return false;
-		}
-		this.hbp = new HBProfileContentForgotPassword((UserRegistration) q.getResultList().get(0));
-		em = null;
-		return true;
-	}
-	
-	private void drawNegativeAnswer() {
-		this.layout.getChildren().clear();
-		this.hbButtons.getChildren().clear();
-		this.hbButtons.getChildren().addAll(this.btnOk, this.btnCancel);
-		
-		this.layout.getChildren().addAll(lblEmail, txtEmail);
-		this.layout.getChildren().addAll(lblNPass, npass);
-		this.layout.getChildren().addAll(lblCNpass, cnpass);
-		this.layout.getChildren().add(this.hbButtons);
-	}
-	
-	private void drawPositiveAnswser() throws IOException {
-		this.layout.getChildren().clear();
-		this.hbButtons.getChildren().clear();
-		this.hbButtons.getChildren().addAll(this.btnOk, this.btnCancel);
-		/*
-		 */
-		UserRegistration u = (UserRegistration) q.getResultList().get(0);
-		lblEmail.setText(u.getEmail());
-		this.img = new ImageView();
-		this.img.setImage(ProfileImg.getImage(u.getProfile()));
-		this.img.setFitHeight(200);
-		this.img.setFitWidth(200);
-		//
-		this.layout.getChildren().add(img);
-		this.layout.getChildren().add(lblEmail);
-		this.layout.getChildren().addAll(lblNPass, npass);
-		this.layout.getChildren().addAll(lblCNpass, cnpass);
-		this.layout.getChildren().add(this.hbButtons);
-	}
-	
-	private boolean valideFieldsEmpty() {
-		if(needEmail) {
-		if (this.checkFields.isTextFieldEmpty(this.txtEmail))
-			this.message.add("email");
-		}
-		if (this.checkFields.isPasswordFieldEmpty(this.npass))
-			this.message.add("senha");
-		if (this.checkFields.isPasswordFieldEmpty(this.cnpass))
-			this.message.add("confirmação de senha");
-		if (!this.message.isEmpty()) {
-			StringBuilder stb = new StringBuilder();
-			
-			if (this.message.size() > 1)
-				stb.append("Os campos ");
-			if (this.message.size() == 1)
-				stb.append("O campo ");
-			
-			for (int i = 0; i < this.message.size(); i++) {
-				stb.append(this.message.get(i));
-				if (this.message.get(i) != null) {
-					stb.append(", ");
-				}
-				int j = i;
-				try {
-					if (this.message.get(j++) != null) {
-						stb.append("\n");
-					}
-				} catch (IndexOutOfBoundsException a) {
-					if (j++ == this.message.size() - 1) {
-						stb.append("\n");
-					}
-				}
-			}
-			if (this.message.size() > 1)
-				stb.append(" estão vazios.");
-			if (this.message.size() == 1)
-				stb.append(" está vazio.");
-			new CustomAlert(AlertType.ERROR, "ERRO", "Campos Vazios", stb.toString());
-			this.message.clear();
-			return true;
-		}
-		return false;
-	}
-	
-	/**
-	 *
-	 * I don't know why i use this shitty logic, 
-	 *	im sorry if you had to read this
-	 * code
-	 * good luck trying to understand
-	 * @param  String email
-	 * @param  String password
-	 * @param  String passwordConfirmation
-	 * @param boolean needEmail
-	 * @author jefter66
-	 * @return
-	 */
-	private boolean changePassword(String email, String password, String passwordConfirmation, boolean needEmail) {
-		Query q = null;
-		if (em == null)
-			em = Database.createEntityManager();
-		if (needEmail) {
-			q = em.createQuery("from UserRegistration where email =: email");
-			q.setParameter("email", email);
-		}
-		if (!needEmail) {
-			q = em.createQuery("from UserRegistration where email =: email");
-			UserRegistration  u  = (UserRegistration) this.q.getResultList().get(0);
-			q.setParameter("email", u.getEmail());
-		}
-		UserRegistration u = null;
-		if (q != null)	u = (UserRegistration) q.getResultList().get(0);
-		
-		if (!needEmail && this.q.getResultList().get(0) != null)	u = (UserRegistration) this.q.getResultList().get(0);
-		
-		
-		
-		if (password.equals(passwordConfirmation)) {
-			em.getTransaction().begin();
-			em.merge(u);
-			em.getTransaction().commit();
-			em.clear();
-			em.close();
-			em = null;
-			return true;
-		}
-		return false;
-	}
 }
+
+	public void valideEmail() { 
+		
+		
+		
+	}
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
