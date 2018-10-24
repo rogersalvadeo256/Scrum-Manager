c0f278117158c64e5c733b4016493336b81e1d9e
Aluno <andrelopessousa8@gmail.com>
2018-10-24 15:10:58 -0300

    consertei o toggle button para o user desativar conta na classe EditProfile

--- src/application/controllers/EditProfileController.java
+++ src/application/controllers/EditProfileController.java
@@ -12,8 +12,6 @@ import application.main.Window;
 import db.hibernate.factory.Database;
 import db.pojos.USER_PROFILE;
 import db.pojos.USER_REGISTRATION;
-import javafx.beans.value.ChangeListener;
-import javafx.beans.value.ObservableValue;
 import javafx.event.ActionEvent;
 import javafx.geometry.Pos;
 import javafx.scene.control.Alert.AlertType;
@@ -22,7 +20,6 @@ import javafx.scene.control.ButtonType;
 import javafx.scene.control.Label;
 import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
-import javafx.scene.control.Toggle;
 import javafx.scene.layout.HBox;
 import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
@@ -33,79 +30,104 @@ import validation.CheckEmptyFields;
 import view.popoups.ProfileEditPOPOUP;
 import view.scenes.LoginScene;
 import widgets.alertMessage.CustomAlert;
-import widgets.designComponents.profileContents.HBStatusBar;
 
 public class EditProfileController {
 
 	private EntityManager em;
 	private CheckEmptyFields check;
-	private Button btnChangeQuestion, btnChangeAnswer;
-	private HBStatusBar hbDeleteAccount;
+	private Button btnDesativar;
+//	private HBStatusBar hbDeleteAccount;
 	private ProfileEditPOPOUP screen;
-	
+
 	public EditProfileController() {
 		this.check = new CheckEmptyFields();
-		this.btnChangeAnswer = new Button("Alterar");
-		this.btnChangeQuestion = new Button("Alterar");
-		this.hbDeleteAccount = new HBStatusBar(false, "Desativar Conta", "Conta Ativa");
+//		this.btnChangeAnswer = new Button();
+//		this.btnChangeQuestion = new Button();
+		this.btnDesativar = new Button("Desativar conta");
+//		this.hbDeleteAccount = new HBStatusBar(false, "Desativar Conta", "Conta Ativa");
 
+		this.btnDesativar.setOnAction(e -> {
+			Optional<ButtonType> result = new CustomAlert(AlertType.WARNING, "Apagar conta", "Sua conta ficará inativa",
+					"Para reativar sua conta vá até a opção \n na tela de login").showAndWait();
 
-		this.hbDeleteAccount.setGroupEvent(new ChangeListener<Toggle>() {
+			if (result.get() == ButtonType.OK) {
+				EntityManager em = Database.createEntityManager();
 
-			@Override
-			public void changed(ObservableValue<? extends Toggle> observable, Toggle oldValue, Toggle newValue) {
+				USER_REGISTRATION u = SESSION.getUserLogged();
 
-				if (newValue.isSelected()) {
+				u.setStatus(ENUMS.ACCOUNT_STATUS.INACTIVE.getValue());
 
-					Optional<ButtonType> result = new CustomAlert(AlertType.WARNING, "Apagar conta", "Sua conta ficará inativa", "Para reativar sua conta vá até a opção na tela de login").showAndWait();
+				em.getTransaction().begin();
+				em.merge(u);
+				em.getTransaction().commit();
+				em.clear();
 
-					if (result.get() == ButtonType.OK) {
-						newValue.setSelected(true);
-						EntityManager em = Database.createEntityManager();
-
-						USER_REGISTRATION u = SESSION.getUserLogged();
-
-						u.setStatus(ENUMS.ACCOUNT_STATUS.INACTIVE.getValue());
-
-						em.getTransaction().begin();
-						em.merge(u);
-						em.getTransaction().commit();
-						em.clear();
-
-						SESSION.RESET();
-						EditProfileController.this.screen.close();
-						try {
-							Window.mainStage.setScene(new LoginScene());
-						} catch (ClassNotFoundException | FileNotFoundException | SQLException e) {
-							e.printStackTrace();
-						}
-						return;
-					}
-					newValue.setSelected(true);
+				SESSION.RESET();
+				EditProfileController.this.screen.close();
+				try {
+					Window.mainStage.setScene(new LoginScene());
+				} catch (ClassNotFoundException | FileNotFoundException | SQLException e2) {
+					e2.printStackTrace();
 				}
+				return;
 			}
 		});
 
+//		this.hbDeleteAccount.setGroupEvent(new ChangeListener<Toggle>() {
+//
+//			@Override
+//			public void changed(ObservableValue<? extends Toggle> observable, Toggle oldValue, Toggle newValue) {
+//
+//				if (newValue.isSelected()) {
+//
+//
+//					if (result.get() == ButtonType.OK) {
+//						newValue.setSelected(true);
+//						EntityManager em = Database.createEntityManager();
+//
+//						USER_REGISTRATION u = SESSION.getUserLogged();
+//
+//						u.setStatus(ENUMS.ACCOUNT_STATUS.INACTIVE.getValue());
+//
+//						em.getTransaction().begin();
+//						em.merge(u);
+//						em.getTransaction().commit();
+//						em.clear();
+//
+//						SESSION.RESET();
+//						EditProfileController.this.screen.close();
+//						try {
+//							Window.mainStage.setScene(new LoginScene());
+//						} catch (ClassNotFoundException | FileNotFoundException | SQLException e) {
+//							e.printStackTrace();
+//						}
+//						return;
+//					}
+//					newValue.setSelected(true);
+//				}
+//			}
+//		});
+//
 	}
 
-
 	/*
-	 * this code is huge and redundant, but was the best way that i find to do this and
-	 * understand later
+	 * this code is huge and redundant, but was the best way that i find to do this
+	 * and understand later
 	 */
-	public void setEventAdvancedOptions(ActionEvent e, ProfileEditPOPOUP screen, VBox layout, Stage stage, HBox hbChangeAnswer, HBox hbChangeQuestion, HBox hbButtons, Button btnBack) {
-		
-		this.screen= screen;
-		
+	public void setEventAdvancedOptions(ActionEvent e, ProfileEditPOPOUP screen, VBox layout, Stage stage,
+			HBox hbChangeAnswer, HBox hbChangeQuestion, HBox hbButtons, Button btnBack) {
+
+		this.screen = screen;
+
 		layout.getChildren().clear();
 		hbChangeAnswer.getChildren().clear();
 		hbChangeQuestion.getChildren().clear();
 
 		hbChangeQuestion.getChildren().add(new Label(SESSION.getUserLogged().getSecurityQuestion().toString()));
 		hbChangeAnswer.getChildren().add(new Label(SESSION.getUserLogged().getSecurityAnswer().toString()));
-
-		hbChangeQuestion.getChildren().add(this.btnChangeQuestion);
-		hbChangeAnswer.getChildren().add(this.btnChangeAnswer);
+//
+//		hbChangeQuestion.getChildren().addAll(this.btnChangeQuestion);
+//		hbChangeAnswer.getChildren().addAll(this.btnChangeAnswer);
 
 		hbChangeAnswer.setAlignment(Pos.CENTER);
 		hbChangeQuestion.setAlignment(Pos.CENTER);
@@ -113,90 +135,76 @@ public class EditProfileController {
 		hbChangeAnswer.setSpacing(20);
 		hbChangeQuestion.setSpacing(20);
 
+		layout.getChildren().add(new Label("Alterar pergunta"));
 		layout.getChildren().addAll(hbChangeQuestion);
+		hbChangeQuestion.setId("hbChangeQuestion");
+		layout.getChildren().add(new Label("Alterar resposta"));
 		layout.getChildren().addAll(hbChangeAnswer);
-		 layout.getChildren().add(hbDeleteAccount);
+		hbChangeAnswer.setId("hbChangeAnswer");
+		layout.getChildren().add(btnDesativar);
+		// layout.getChildren().add(hbDeleteAccount);
 		layout.getChildren().add(hbButtons);
 		layout.setSpacing(20);
-
-		this.btnChangeQuestion.setOnAction(e1 -> {
-
-			hbChangeQuestion.getChildren().clear();
-
-			TextField txtQuestion = new TextField();
-			Button btnCancel = new Button("Cancelar");
-			Button btnChange = new Button("Alterar");
-
-			txtQuestion.setPromptText(SESSION.getUserLogged().getSecurityQuestion().toString());
-
-			hbChangeQuestion.getChildren().addAll(txtQuestion, btnChange, btnCancel);
-
-			btnChange.setOnAction(e2 -> {
-				if (em == null)
-					em = Database.createEntityManager();
-				USER_REGISTRATION u = SESSION.getUserLogged();
-				if (!check.isTextFieldEmpty(txtQuestion)) {
-
-					u.setSecurityQuestion(txtQuestion.getText());
-					em.getTransaction().begin();
-					em.merge(u);
-					em.getTransaction().commit();
-					em.clear();
-					em.close();
-					em = null;
-					SESSION.UPDATE_SESSION();
-					hbChangeQuestion.getChildren().clear();
-
-					hbChangeQuestion.getChildren().add(new Label(SESSION.getUserLogged().getSecurityQuestion().toString()));
-					hbChangeQuestion.getChildren().add(btnChangeQuestion);
-				}
-			});
-			btnCancel.setOnAction(e3 -> {
-				hbChangeQuestion.getChildren().clear();
-				hbChangeQuestion.getChildren().add(new Label(SESSION.getUserLogged().getSecurityQuestion().toString()));
-				hbChangeQuestion.getChildren().add(btnChangeQuestion);
-			});
-
-		});
-		this.btnChangeAnswer.setOnAction(e1 -> {
-
-			hbChangeAnswer.getChildren().clear();
-			TextField txtAnswer = new TextField();
-			Button btnCancel = new Button("Cancelar");
-			Button btnChange = new Button("Alterar");
-
-			txtAnswer.setPromptText(SESSION.getUserLogged().getSecurityAnswer().toString());
-
-			hbChangeAnswer.getChildren().addAll(txtAnswer, btnChange, btnCancel);
-
-			btnChange.setOnAction(e2 -> {
-				if (em == null)
-					em = Database.createEntityManager();
-				USER_REGISTRATION u = SESSION.getUserLogged();
-				if (!check.isTextFieldEmpty(txtAnswer)) {
-					u.setSecurityAnswer(txtAnswer.getText());
-					em.getTransaction().begin();
-					em.merge(u);
-					em.getTransaction().commit();
-					em.clear();
-					em.close();
-					em = null;
-					SESSION.UPDATE_SESSION();
-					hbChangeAnswer.getChildren().clear();
-					hbChangeAnswer.getChildren().add(new Label(SESSION.getUserLogged().getSecurityAnswer().toString()));
-					hbChangeAnswer.getChildren().add(btnChangeAnswer);
-				}
-			});
-			btnCancel.setOnAction(e3 -> {
-				hbChangeAnswer.getChildren().clear();
-				hbChangeAnswer.getChildren().add(new Label(SESSION.getUserLogged().getSecurityAnswer().toString()));
-				hbChangeAnswer.getChildren().add(btnChangeAnswer);
-			});
-		});
-		backToNormalOptions(e, btnBack, screen);
 	}
+	/*
+	 * this.btnChangeQuestion.setOnAction(e1 -> {
+	 * 
+	 * hbChangeQuestion.getChildren().clear();
+	 * 
+	 * TextField txtQuestion = new TextField(); Button btnCancel = new
+	 * Button("Cancelar"); Button btnChange = new Button("Alterar");
+	 * 
+	 * txtQuestion.setPromptText(SESSION.getUserLogged().getSecurityQuestion().
+	 * toString());
+	 * 
+	 * hbChangeQuestion.getChildren().addAll(txtQuestion, btnChange, btnCancel);
+	 * 
+	 * btnChange.setOnAction(e2 -> { if (em == null) em =
+	 * Database.createEntityManager(); USER_REGISTRATION u =
+	 * SESSION.getUserLogged(); if (!check.isTextFieldEmpty(txtQuestion)) {
+	 * 
+	 * u.setSecurityQuestion(txtQuestion.getText()); em.getTransaction().begin();
+	 * em.merge(u); em.getTransaction().commit(); em.clear(); em.close(); em = null;
+	 * SESSION.UPDATE_SESSION(); hbChangeQuestion.getChildren().clear();
+	 * 
+	 * hbChangeQuestion.getChildren().add(new
+	 * Label(SESSION.getUserLogged().getSecurityQuestion().toString()));
+	 * hbChangeQuestion.getChildren().addAll(btnChangeQuestion); } });
+	 * 
+	 * btnCancel.setOnAction(e3 -> { hbChangeQuestion.getChildren().clear();
+	 * hbChangeQuestion.getChildren().add(new
+	 * Label(SESSION.getUserLogged().getSecurityQuestion().toString()));
+	 * hbChangeQuestion.getChildren().add(btnChangeQuestion); });
+	 * 
+	 * }); this.btnChangeAnswer.setOnAction(e1 -> {
+	 * 
+	 * hbChangeAnswer.getChildren().clear(); TextField txtAnswer = new TextField();
+	 * Button btnCancel = new Button("Cancelar"); Button btnChange = new
+	 * Button("Alterar");
+	 * 
+	 * txtAnswer.setPromptText(SESSION.getUserLogged().getSecurityAnswer().toString(
+	 * ));
+	 * 
+	 * hbChangeAnswer.getChildren().addAll(txtAnswer, btnChange, btnCancel);
+	 * 
+	 * btnChange.setOnAction(e2 -> { if (em == null) em =
+	 * Database.createEntityManager(); USER_REGISTRATION u =
+	 * SESSION.getUserLogged(); if (!check.isTextFieldEmpty(txtAnswer)) {
+	 * u.setSecurityAnswer(txtAnswer.getText()); em.getTransaction().begin();
+	 * em.merge(u); em.getTransaction().commit(); em.clear(); em.close(); em = null;
+	 * SESSION.UPDATE_SESSION(); hbChangeAnswer.getChildren().clear();
+	 * hbChangeAnswer.getChildren().add(new
+	 * Label(SESSION.getUserLogged().getSecurityAnswer().toString()));
+	 * hbChangeAnswer.getChildren().addAll(btnChangeAnswer); } });
+	 * btnCancel.setOnAction(e3 -> { hbChangeAnswer.getChildren().clear();
+	 * hbChangeAnswer.getChildren().add(new
+	 * Label(SESSION.getUserLogged().getSecurityAnswer().toString()));
+	 * hbChangeAnswer.getChildren().add(btnChangeAnswer); }); });
+	 * backToNormalOptions(e, btnBack, screen); }
+	 */
 
-	public void setEventFinish(ActionEvent e, TextField txtName, PasswordField txtCurrentPassword, PasswordField txtNewPassword) {
+	public void setEventFinish(ActionEvent e, TextField txtName, PasswordField txtCurrentPassword,
+			PasswordField txtNewPassword) {
 		USER_REGISTRATION u = SESSION.getUserLogged();
 		USER_PROFILE p = SESSION.getProfileLogged();
 		if (em == null)
@@ -212,7 +220,8 @@ public class EditProfileController {
 					em.getTransaction().commit();
 					em.clear();
 				} else {
-					new CustomAlert(AlertType.ERROR, "Erro", "Senha muito curta", "A senha deve conter no minimo 8 caracteres");
+					new CustomAlert(AlertType.ERROR, "Erro", "Senha muito curta",
+							"A senha deve conter no minimo 8 caracteres");
 					return;
 				}
 			}
@@ -250,18 +259,20 @@ public class EditProfileController {
 			});
 		});
 	}
-	
+
 	/*
 	 * 
 	 * not used anymore
 	 */
 	@SuppressWarnings("unused")
-	private void deleteAccount(ActionEvent e, Stage stage) throws ClassNotFoundException, FileNotFoundException, SQLException {
+	private void deleteAccount(ActionEvent e, Stage stage)
+			throws ClassNotFoundException, FileNotFoundException, SQLException {
 
 		if (this.em == null)
 			this.em = Database.createEntityManager();
 
-		Optional<ButtonType> result = new CustomAlert(AlertType.WARNING, "Apagar conta", "Sua conta será apagada", "Todos os seus dados serão apagados").showAndWait();
+		Optional<ButtonType> result = new CustomAlert(AlertType.WARNING, "Apagar conta", "Sua conta será apagada",
+				"Todos os seus dados serão apagados").showAndWait();
 
 		if (result.get() == ButtonType.OK) {
 
--- src/widgets/alertMessage/MessageDialog.java
+++ src/widgets/alertMessage/MessageDialog.java
@@ -6,8 +6,11 @@ public class MessageDialog  extends DialogPane{
 	String dialogTitle;	
 	String dialogHeader;
 	String dialogContent;
+	
+	
 	public MessageDialog() {
 		this.computeMinHeight(200);
 		this.computeMinWidth(200);
+		this.getStylesheets().add(this.getClass().getResource("/css/DIALOG_PANE2.css").toExternalForm());
 	}
 }
